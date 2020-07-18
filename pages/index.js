f7e984838e23d5ef2762675d457f6528549a8cdc
RandyWardhana <randywardhana37@gmail.com>
2020-07-18 22:45:25 +0700

    Index Page for Login

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,97 @@
-import Head from 'next/head'
+import React from 'react'
+import { Container, Paper, Typography, Grid, Button, TextField } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { MoreHoriz } from '@material-ui/icons'
+import { useRouter } from 'next/router'
+import Axios from 'axios'
+import _ from 'lodash'
 
-export default function Home() {
+import Meta from '../lib/Meta'
+import baseURL from '../utils/baseURL'
+
+function Home() {
+  const useStyles = makeStyles((theme) => ({
+    container: {
+      marginTop: 72
+    },
+    containerLogin: {
+      alignItems: 'center',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      padding: 32,
+    },
+    input: {
+      margin: '8px 0'
+    },
+    errorText: {
+      color: 'red',
+      fontSize: 12,
+      margin: '8px 0',
+    }
+  }))
+
+  const classes = useStyles()
+  const route = useRouter()
+
+  const [username, setUsername] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  const [error, setError] = React.useState('')
+  const [loading, setLoading] = React.useState(false)
+
+  React.useEffect(() => {
+    let token = localStorage.getItem('token')
+    if (!_.isEmpty(token)) route.push('/settings/roles/hierarchy-structure')
+  })
+
+  const validate = async () => {
+    setError('')
+    setLoading(true)
+    const data = { username, password }
+
+    try {
+      let res = await Axios.post(`${baseURL}/v1/auth`, data)
+      setLoading(false)
+      if (res.status == 200) {
+        localStorage.setItem('token', res.data.payload.token)
+        return route.push('/settings/roles/hierarchy-structure')
+      }
+    } catch (e) {
+      setLoading(false)
+      setError(e.response.data.description)
+    }
+  }
+
+  const renderInput = (className, label, type, value, onChange) => (
+    <TextField
+      className={className}
+      required
+      label={label}
+      type={type}
+      onChange={onChange}
+      value={value}
+      fullWidth
+      variant='outlined' />
+  )
+
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Container>
+      <Meta />
+      <Grid container className={classes.container}>
+        <Grid item xs={12}>
+          <Paper className={classes.containerLogin} elevation={2}>
+            <Typography>Login</Typography>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+            {renderInput(classes.input, 'Username', 'text', username, (e) => setUsername(e.target.value))}
+            {renderInput(classes.input, 'Password', 'password', password, (e) => setPassword(e.target.value))}
+            {!_.isEmpty(error) && <Typography className={classes.errorText}>{error}</Typography>}
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+            <Button variant="contained" color="primary" onClick={() => validate()}>{loading ? <MoreHoriz /> : 'Masuk'}</Button>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
   )
 }
+
+export default Home
